bfcecd2aead835fdc11afba234e242c280ee333b
p1nero <1070402601@qq.com>
2025-10-13 03:34:40 +0800

    改描述部分文本颜色

--- src/main/java/com/p1nero/tcrcore/client/gui/HandleArteriusDialog.java
+++ src/main/java/com/p1nero/tcrcore/client/gui/HandleArteriusDialog.java
@@ -8,6 +8,7 @@ import com.p1nero.dialog_lib.client.screen.DialogueScreenBuilder;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.resources.language.I18n;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.animal.IronGolem;
@@ -23,21 +24,21 @@ public class HandleArteriusDialog {
         DialogueScreenBuilder treeBuilder = new DialogueScreenBuilder(self);
         DialogueComponentBuilder componentBuilder = treeBuilder.getComponentBuildr();
         DialogNode root;
-        if(arteriusKilled) {
+        if (arteriusKilled) {
             root = treeBuilder.newNode(2)
                     .addChild(treeBuilder.newNode(3, 4)
                             .addChild(treeBuilder.newFinalNode(2, 1))
                             .addChild(treeBuilder.newFinalNode(3))
                     )
                     .addChild(new DialogNode(componentBuilder.ans(4,
-                            ModEntities.NETHERITE_MONSTROSITY.get().getDescription().copy().withStyle(ChatFormatting.RED),
-                            EFNItem.DUSKFIRE_INGOT.get().getDescription().copy().withStyle(ChatFormatting.GOLD),
-                            EFNItem.DUSKFIRE_INGOT.get().getDescription().copy().withStyle(ChatFormatting.GOLD)), componentBuilder.opt(5))
+                            ("§c" + I18n.get(ModEntities.NETHERITE_MONSTROSITY.get().getDescriptionId()) + "§f"),
+                            ("§6" + I18n.get(EFNItem.DUSKFIRE_INGOT.get().getDescriptionId()) + "§f"),
+                            ("§6" + I18n.get(EFNItem.DUSKFIRE_INGOT.get().getDescriptionId()) + "§f")), componentBuilder.opt(5))
                             .addChild(new DialogNode(componentBuilder.ans(5,
-                                    EntityType.ENDER_DRAGON.getDescription().copy().withStyle(ChatFormatting.LIGHT_PURPLE),
-                                    EntityType.WITHER.getDescription().copy().withStyle(ChatFormatting.RED),
-                                    ModEntities.ENDER_GUARDIAN.get().getDescription().copy().withStyle(ChatFormatting.LIGHT_PURPLE),
-                                    ModEntities.THE_HARBINGER.get().getDescription().copy().withStyle(ChatFormatting.DARK_RED)), componentBuilder.opt(6))
+                                    ("§d" + I18n.get(EntityType.ENDER_DRAGON.getDescriptionId()) + "§f"),
+                                    ("§c" + I18n.get(EntityType.WITHER.getDescriptionId()) + "§f"),
+                                    ("§d" + I18n.get(ModEntities.ENDER_GUARDIAN.get().getDescriptionId()) + "§f"),
+                                    ("§4" + I18n.get(ModEntities.THE_HARBINGER.get().getDescriptionId())) + "§f"), componentBuilder.opt(6))
                                     .addLeaf(componentBuilder.opt(7)))
                     )
 
