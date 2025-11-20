8b9cbab2b24143b0a65215befa193768b31d3135
p1nero <1070402601@qq.com>
2025-11-21 00:32:38 +0800

    弓耐久

--- src/main/java/com/p1nero/tcrcore/mixin/ItemStackMixin.java
+++ src/main/java/com/p1nero/tcrcore/mixin/ItemStackMixin.java
@@ -1,7 +1,5 @@
 package com.p1nero.tcrcore.mixin;
 
-import com.github.L_Ender.cataclysm.init.ModItems;
-import net.minecraft.world.item.BowItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
- * 移除物品耐久度，弓除外
+ * 移除物品耐久度
  */
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
@@ -21,10 +19,6 @@ public abstract class ItemStackMixin {
 
     @Inject(method = "isDamageableItem", at = @At("HEAD"), cancellable = true)
     private void tcr$damageable(CallbackInfoReturnable<Boolean> cir) {
-        if(this.getItem() instanceof BowItem || this.getItem() == ModItems.WRATH_OF_THE_DESERT.get() || this.getItem() == ModItems.CURSED_BOW.get()) {
-            cir.setReturnValue(true);
-        } else {
-            cir.setReturnValue(false);
-        }
+        cir.setReturnValue(false);
     }
 }
